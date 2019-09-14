61ac9cb3936a08a2df928eaf3bd1c520a3f82c96
Kurmuro <55317081+Kurmuro@users.noreply.github.com>
2019-09-14 13:42:07 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+from random import shuffle
+
+Liste = "Super Toller Extremer Harter Cooler Nerviger".upper().split()
+shuffle(Liste)
+
+
+for strophe in range(2):
+    for g in range(2):
+        for i in range(4):
+            print("SPAM", end=' ')
+        print()
+    print(Liste.pop() +" SPAM", Liste.pop() +" SPAM")
+    print()
